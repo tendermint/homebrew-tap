4ae3e7a901f7532a6acf50baeeae65db563a863d
Denis Fadeev <denis@fadeev.org>
2020-08-21 23:56:29 +0500

    Update starport.rb

--- starport.rb
+++ starport.rb
@@ -6,11 +6,11 @@ class Starport < Formula
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/tendermint/starport/releases/download/0.0.10-rc.3/starport_0.0.10-rc.3_darwin_amd64.tar.gz"
+    url "https://github.com/tendermint/starport/releases/download/v0.0.10-rc.3/starport_0.0.10-rc.3_darwin_amd64.tar.gz"
     sha256 "085bcc12f26bc3b385f15f106feaee947e03c60f944518a1a3823eaf6cac28f3"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/tendermint/starport/releases/download/0.0.10-rc.3/starport_0.0.10-rc.3_linux_amd64.tar.gz"
+      url "https://github.com/tendermint/starport/releases/download/v0.0.10-rc.3/starport_0.0.10-rc.3_linux_amd64.tar.gz"
       sha256 "dfed8de3d12c74afc910ef55753f2b901f71dc9b018c68b35187ebbd6b272583"
     end
   end
