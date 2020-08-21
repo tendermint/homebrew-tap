0decd003c82c1157fd666825e7eb05f954df416e
goreleaserbot <goreleaser@carlosbecker.com>
2020-08-21 17:29:14 +0500

    Brew formula update for starport version 0.0.10-rc.3

--- starport.rb
+++ starport.rb
@@ -7,11 +7,11 @@ class Starport < Formula
 
   if OS.mac?
     url "https://github.com/tendermint/starport/releases/download/0.0.10-rc.3/starport_0.0.10-rc.3_darwin_amd64.tar.gz"
-    sha256 "df22c61e72ed6869609163f355c16a0e53a9babd80ee27b931afd2494a99d0d4"
+    sha256 "085bcc12f26bc3b385f15f106feaee947e03c60f944518a1a3823eaf6cac28f3"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/tendermint/starport/releases/download/0.0.10-rc.3/starport_0.0.10-rc.3_linux_amd64.tar.gz"
-      sha256 "5255b2ba7acb216e4356e6c6f1fc8db7183c9758a9ff48c4f18a25260ba060e3"
+      sha256 "dfed8de3d12c74afc910ef55753f2b901f71dc9b018c68b35187ebbd6b272583"
     end
   end
 
